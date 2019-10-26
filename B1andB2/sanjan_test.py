a0bd2cab44ae996cfb153dc5a7eb95ce7239e3d2
Sanjan Das <sanjandazz@gmail.com>
2019-10-26 11:44:57 +0100

    lala

--- B1andB2/sanjan_test.py
+++ B1andB2/sanjan_test.py
@@ -1,2 +1,12 @@
-import cv2 as cv
 
+#import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+#reading the image 
+
+image = cv2.imread('samplepic.png')
+image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+#plotting the image
+plt.imshow(image)
