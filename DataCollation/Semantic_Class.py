cd07f6acc0b9aeb3d1b3dbac8a6eacee34b6a16a
Tomos <tww26@cam.ac.uk>
2019-10-26 18:29:59 +0100

    Conditions function works!
    Love Tomos

--- DataCollation/Semantic_Class.py
+++ DataCollation/Semantic_Class.py
@@ -84,7 +84,53 @@ class LCDDisplay(Semantic):
         self._value = state
  
 
-### TESTS        
+### TESTS  
+        
+def ConditionCheck(conditions_map, semantic_map):
+    """
+    TERMINOLOGY
+    [Semantic Object: a dial of sorts; one of the three inherited classes of "Semantic"]
+    [Semantic Map: a dictionary of this form, created at configuration {OutputID:Semantic Object}]
+    
+    Preferred input*: 
+        {OutputID:Conditions}
+        {OutputID:Semantic Objects}
+    The idea is that this would be produced alongside an {OutputID:Semantic Object} at the start
+    """
+    
+    conditions = {} # An empty {Semantic Object:Conditions} object
+    for i in conditions_map:
+        # i becomes key, and we retain the same value as in semantic_map dictionary
+        conditions[semantic_map[i]] = conditions_map[i] 
+        
+    Result = []
+    count = 0
+    for i in conditions:
+        
+        Result.append(0)
+        
+        # If i represents a continuous dial
+        if type(conditions[i])==list:
+            
+            if conditions[i][0]==False & conditions[i][1]==True: # Only an upper limit
+                Result[count] = (i.value>conditions[i][1]) 
+                
+            if conditions[i][1]==False & conditions[i][0]==True: # Only a lower limit
+                Result[count] = (i.value<conditions[i][0]) 
+                
+            else:   # allowable RANGE
+                Result[count] = (i.value<conditions[i][0])or(i.value>conditions[i][1]) 
+                
+        # If i represents a discrete dial where True is already mapped to a string e.g. "Off"
+        if type(conditions[i])==str:
+            Result[count]=(i.value.upper()==conditions[i].upper())
+            
+        count += 1
+        
+    if Result.count(True)==len(Result):
+        return True
+    else:
+        return False
    
 if __name__=="__main__":
     
@@ -101,16 +147,17 @@ if __name__=="__main__":
     Needle_test.value = 10
     print(Needle_test.meaning, Needle_test.value)
     
+    Example_semanticmap   = {"ID1":Needle_test,"ID2":test,"ID3":Switchvalue}
+    Example_conditionsmap = {"ID1":[5,25],"ID2":"On","ID3":"middle"}
     
-    Conditions = {Needle_test:[5,35],test:"On"} # Test conditions 
-    Result = {}
+    #ConditionsMap = {Needle_test:[5,35],test:"On",Switchvalue:"middle"} # Test conditions 
     
-    for i in Conditions:
-        if type(Conditions[i])==list:
-            Result[i]=(i.value<Conditions[i][0])or(i.value>Conditions[i][1])
-        if type(Conditions[i])==str:
-            Result[i]=(i.value.upper()==Conditions[i].upper())
+    print(ConditionCheck(Example_conditionsmap,Example_semanticmap))
+    
+    
+
             
+    
             
 
     
--- E3/Fault_Conditions.py
+++ E3/Fault_Conditions.py
@@ -1,10 +1,59 @@
 # -*- coding: utf-8 -*-
-Condition1 = {"id1":[False,10], "id2": True, "id3":[5,32]}
-Example_SemanticMap = {"id1":Semantic_object1:"id2":Semantic_object2, "id3":Semantic_obect3}
 
-def check_error(ConditionMap,SemanticMap):
-    """Checks a map of errors against a map of semantic objects with states 
-    assumes that ID keys are consistently named"""
+# USE THESE ON SEMANTIC CLASS MODULE IF U WANNA TEST
+#Example_semanticmap   = {"ID1":Needle_test,"ID2":test,"ID3":Switchvalue}
+#Example_conditionsmap = {"ID1":[5,25],"ID2":"On","ID3":"middle"}
+
+
+def ConditionCheck(conditions_map, semantic_map):
+    """
+    TERMINOLOGY
+    [Semantic Object: a dial of sorts; one of the three inherited classes of "Semantic"]
+    [Semantic Map: a dictionary of this form, created at configuration {OutputID:Semantic Object}]
     
+    Preferred input*: 
+        {OutputID:Conditions} with as many SIMULTANEOUS conditions as needed
+        {OutputID:Semantic Objects}
+        
+    For objects with continuous values (continuous dials), the condition should be [lower limit,upper limit]
+        It will give True if the value is below a lower limit or above the upper
+        Replace l limit with False if you don't want a lower limit
+    For objects with string conditions "On", just give "On". Capitalisation inconsistencies don't matter
     
+    The idea is that this would be produced alongside an {OutputID:Semantic Object} at the start
+    """
+    
+    conditions = {} # An empty {Semantic Object:Conditions} object
+    for i in conditions_map:
+        # i becomes key, and we retain the same value as in semantic_map dictionary
+        conditions[semantic_map[i]] = conditions_map[i] 
+        
+    Result = []
+    count = 0
+    for i in conditions:
+        
+        Result.append(0)
+        
+        # If i represents a continuous dial
+        if type(conditions[i])==list:
+            
+            if conditions[i][0]==False & conditions[i][1]==True: # Only an upper limit
+                Result[count] = (i.value>conditions[i][1]) 
+                
+            if conditions[i][1]==False & conditions[i][0]==True: # Only a lower limit
+                Result[count] = (i.value<conditions[i][0]) 
+                
+            else:   # allowable RANGE
+                Result[count] = (i.value<conditions[i][0])or(i.value>conditions[i][1]) 
+                
+        # If i represents a discrete dial where True is already mapped to a string e.g. "Off"
+        if type(conditions[i])==str:
+            Result[count]=(i.value.upper()==conditions[i].upper())
+            
+        count += 1
+        
+    if Result.count(True)==len(Result):
+        return True
+    else:
+        return False
         
