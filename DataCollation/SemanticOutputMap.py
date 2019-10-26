0e11c76ec585464c40ab116ea6bcdf032552c4cd
Greedquest <guido_spam@hotmail.com>
2019-10-26 13:44:00 +0100

    Add random selection function

--- DataCollation/SemanticOutputMap.py
+++ DataCollation/SemanticOutputMap.py
@@ -4,12 +4,15 @@ Created on Sat Oct 26 11:54:25 2019
 
 @author: Guy
 """
+import random
+import Semantic_Class as semantics
+from abc import ABC, abstractmethod
 
 class SemanticMap():
     
     def __init__(self, mapDict = None):
         
-        if mapDict = None:
+        if mapDict == None:
             self._generateDummyMapping
         
         else:
@@ -18,5 +21,15 @@ class SemanticMap():
     def _generateDummyMapping(self, dataSize = 5):
         
         self._mapDict = {}
+        for i in range(dataSize):
+            self._mapDict[i] = RandomSemanticClass
         
+def RandomSemanticClass():
+    possibleTypes = (semantics.Binary("button{0}".format(random.randint(1,100))),
+                     semantics.LCDDisplay("LCD{0}".format(random.randint(1,100))),)
+    
+    return random.choice(possibleTypes)
+
         
+if __name__ == '__main__':
+    print(RandomSemanticClass().name)
