901d30c3811d49babc0de4bcb28c397ad49bb14f
Bernardo Lustrini <bl434@cam.ac.uk>
2019-10-26 19:14:23 +0100

    made silver switch work

--- B3/silver_switch.py
+++ B3/silver_switch.py
@@ -5,6 +5,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import json
+import os
 
 # preprocess images to a standard form
 def preprocess(image):
@@ -26,12 +27,19 @@ def preprocess(image):
 
     circles = cv2.HoughCircles(image_blur,cv2.HOUGH_GRADIENT,1,20,
                                 param1=50,param2=30,minRadius=0,maxRadius=0)
-    circles = np.uint16(np.around(circles))
+    blank = np.zeros((dsize[1],dsize[0]),np.uint8)
+    try:
+        circles = np.uint16(np.around(circles))
+        circles = np.uint16(np.around(circles))
+
+        i = circles[0,0]
+        centre = (i[0],i[1])
+    except:
+        centre = (dsize[1]//2,dsize[0]//2)
+        i = [0,0,dsize[0]//3]
 
     
-    blank = np.zeros((dsize[1],dsize[0]),np.uint8)
-    i = circles[0,0]        #biggest circle
-    cv2.circle(blank,(i[0],i[1]),i[2],1,-1)   
+    cv2.circle(blank,centre,i[2],1,-1)   
 
 
     masked = cv2.bitwise_and(image, image, mask=blank)  #using a circular mask
@@ -50,7 +58,7 @@ def preprocess(image):
     resized_image = cv2.resize(roi, new_size)
     
     # threshold to binary 
-    ret, final_image = cv2.threshold(resized_image,20,255,cv2.THRESH_BINARY_INV)
+    ret, final_image = cv2.threshold(resized_image,45,255,cv2.THRESH_BINARY_INV)
     
     return final_image
 
@@ -78,14 +86,16 @@ def difference(image1, image2):
     return np.sum(abs(image2-image1))
 
 # import raw images as grayscale
-knob_up_raw = cv2.imread('knob_up.png', 0)
-knob_down_raw = cv2.imread('knob_down.png', 0)
-test_raw = cv2.imread('knob_up.png', 0)
+# knob_up_raw = cv2.imread('knob_up.png', 0)
+# knob_down_raw = cv2.imread('knob_down.png', 0)
+# test_raw = cv2.imread('knob_up.png', 0)
 
 # process images
-processed_up = preprocess(knob_up_raw)
-processed_down = preprocess(knob_down_raw)
-processed_test = preprocess(test_raw)
+# processed_up = preprocess(knob_up_raw)
+# processed_down = preprocess(knob_down_raw)
+# processed_test = preprocess(test_raw)
+
+
 
 
 
@@ -101,8 +111,25 @@ else:
     print('test = down')
 '''
 
-cv2.imshow("knob_up_raw", knob_up_raw)
-cv2.imshow("processed_up", processed_up)
+if __name__ == '__main__':
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    for filename in os.listdir('images/silver_switch/'):
+        print(filename)
+        raw_switch = cv2.imread('images/silver_switch/'+filename,0)
+        raw_switch = preprocess(raw_switch)
+        flipped = np.flip(np.flip(raw_switch,0),1)
+
+        combo = np.logical_and(flipped,raw_switch)
+        xor = np.logical_xor(combo,raw_switch).astype(np.uint8)*255
+        vals = np.nonzero(xor)[0]
+        mean = np.mean(vals)
+
+        if mean < np.shape(xor)[0]/2:
+            print('down')
+        else:
+            print('up')
+
+        cv2.imshow('h',xor)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
