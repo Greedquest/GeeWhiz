15b7bdc777a1d24fc8c2fa9e0bf804fdf22f5f56
Greedquest <guido_spam@hotmail.com>
2019-10-26 12:09:35 +0100

    Semantic Map class
    This controls how data is interpreted from machine vision

---
+++ DataCollation/SemanticOutputMap.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 11:54:25 2019
+
+@author: Guy
+"""
+
+class SemanticMap():
+    
+    def __init__(self, mapDict = None):
+        
+        if mapDict = None:
+            self._generateDummyMapping
+        
+        else:
+            self._mapDict = mapDict
+    
+    def _generateDummyMapping(self, dataSize = 5):
+        
+        self._mapDict = {}
