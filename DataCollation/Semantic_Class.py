6158f412849cdf44406bce1a90027e3414fac4ac
Tomos <tww26@cam.ac.uk>
2019-10-26 13:56:17 +0100

    Updates
    New second generation classes added with some binary functionality

--- DataCollation/Semantic_Class.py
+++ DataCollation/Semantic_Class.py
@@ -11,37 +11,84 @@ class Semantic(ABC):
         return self._value
     
     @value.setter
-    def value(self, state):
-        self._valueSetter
-        
     @abstractmethod # Inherited classes must overwrite this or else run-time error
-    def _valueSetter(self,state):
-        pass  
+    def value(self, state):
+        pass 
     
-# Inherited classes
+### INHERITED CLASSES ###
+        
 class Binary(Semantic):
     "Binary components on the panel, including switches and lights"
     
-    def _valueSetter(self,state):
-        self._value = state
+    def __init__(self,name,valueMap={True:"On",False:"Off"}):
+        self._valueMap = valueMap
+        super(Binary,self).__init__(name)
+    
+    @Semantic.value.setter
+    def value(self,state):
+        self._value = self._valueMap[state]
+        
+    
         
 class NState(Semantic):
     "Dial that has N values on the dial"
     
+    @Semantic.value.setter
     def value(self,state):
-        pass
+        self._value = state
         
 class ContinuousDial(Semantic):
     "Dial that can rotate the full 360 degrees"
+    
+    @Semantic.value.setter
+    def value(self,state):
+        self._value = state
         
 class NeedleDial(Semantic):
     "Needle dials that have a range of "
+    
+    @Semantic.value.setter
+    def value(self,state):
+        self._value = state
         
 class LCDDisplay(Semantic):
     "Parent class that covers word or number LCD displays"
+    
+    @Semantic.value.setter
+    def value(self,state):
+        self._value = state
+        
+### NEXT GENERATION INHERITED CLASSES
+        
+class Switch(Binary):
+    "The up/down switches"
+    
+    @Semantic.value.setter
+    def value(self,state):
+        self._value = state
+    
+class Button(Binary):
+    "The big buttons that can be red/green"
+    
+    
+class LED(Binary):
+    "On/Off light switches"
+    
+        
+class LCDNumerical(LCDDisplay):
+    "The LCD screens that display numerical data"
+
+        
+class LCDText(LCDDisplay):
+    "The LCD screens that display rolling text"
+    
+
+        
+        
    
 if __name__=="__main__":
-    test = Binary("Barry")
-    test.value = "Gill"
+    test = Binary("Barry", {True:"On",False:"Off"})
+    test.value = True
     print(test.name, test.value)
+    
     
