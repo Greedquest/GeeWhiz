204977574a174ad0b1f40d824a70f4f4270083be
Greedquest <guido_spam@hotmail.com>
2019-10-27 13:46:24 +0000

    Added camera stuff

--- GeeWhiz.py
+++ GeeWhiz.py
@@ -3,23 +3,24 @@ from time import sleep
 from ImageProcessing import update_so_values, get_so_list
 from E3.Fault_Conditions import ConditionCheck as check_for_error
 from Email import dispatchFaultMessage
+from camCapture import camCapture
+
 
 oldValues = {}
-
-def generateDummyValues(dummyMap):
-    pass
 
 class Runner:
     def __init__(self, dummyRun=True):
         self._useDummyData = dummyRun
         if dummyRun:
-            self.refreshDelay = 2.4; #seconds
-        self.conditionMaps = []
+            self.refreshDelay = 2.4
+            # seconds
+        self.imagePath = "samplepic.png"
 
     def Run(self):
         ###Get user input - get all the calibrations and initialisations
         # define scope
-        self.generateOutputMapping(self._useDummyData)
+        camCapture(self.imagePath)
+        self.generateOutputMapping()
         self.defineDatabaseConnection()
         self.setFaultConditions()
 
@@ -31,39 +32,47 @@ class Runner:
                 break
             else:
                 sleep(self.refreshDelay)
+                
+            camCapture(self.imagePath)
 
         print("Done!")
         print(self.semanticMap)
+        
+    def defineDatabaseConnection(self):
+        define_log(self.semanticMap.values())
 
-    def generateOutputMapping(self, useDummyData=True):
+    def writeToDatabase(self):
+        write_to_log(self.semanticMap.values())
+
+    def generateOutputMapping(self):
         "Generate the mapping between Semantic objects and their ids"
         # do all the b1 b2 stuff here
-        #get a list of semantic objects to pass
-        self.semanticMap = SemanticMap(None if useDummyData else get_so_list('samplepic_cropped.png'))
-        
-    def updateValues(self):
-        #read new values
-        generateDummyValues(self.semanticMap)
+        # get a list of semantic objects to pass
+        self.semanticMap = SemanticMap(
+            None if self._useDummyData else get_so_list("samplepic_cropped.png")
+        )
 
-    def defineDatabaseConnection(self):
-        pass
+    def updateValues(self):
+        # read new values
+
+        update_so_values(self.semanticMap.values(), self.imagePath)
+
+
 
     def setFaultConditions(self):
-        #generate condition map
+        # generate condition maps
+        self.conditionMaps = []
         pass
-    
+
     def checkForErrors(self):
         for conditionMap in self.conditionMaps:
             if check_for_error(conditionMap, self.semanticMap):
-                #send error alert
-                dispatchFaultMessage(conditionMap,self.semanticMap)
+                # send error alert
+                dispatchFaultMessage(conditionMap, self.semanticMap)
                 anyError = True
-                
+
         return anyError
 
-    
-    def writeToDatabase(self):
-        pass
 
 if __name__ == "__main__":
     runner = Runner(True)
